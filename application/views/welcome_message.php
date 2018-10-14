c5f92a69d5bdc6d22a602b1ecdfa22be1adc6b5c
Feddy <feddy_7990@hotmail.com>
2018-10-14 20:12:34 -0300

    Nuevo Diseño 0.0000001

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
 	<meta charset="utf-8">
@@ -10,51 +10,211 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
 	 crossorigin="anonymous"></script>
-	<script src="semantic/semantic.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="resources/css/app.css">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body>
-	<div class="ui menu">
-		<div class="header item">
-			PokeRancher
-		</div>
-		<a class="item active">
-			Inicio
-		</a>
-		<div class="ui simple dropdown item">
-        Comunidad <i class="dropdown icon"></i>
-        <div class="menu">
-          <a class="item" href="#">Link Item</a>
-          <a class="item" href="#">Link Item</a>
-          <div class="divider"></div>
-          <div class="header">Header Item</div>
-          <div class="item">
-            <i class="dropdown icon"></i>
-            Sub Menu
-            <div class="menu">
-              <a class="item" href="#">Link Item</a>
-              <a class="item" href="#">Link Item</a>
-            </div>
-          </div>
-          <a class="item" href="#">Link Item</a>
-        </div>
-      </div>
-		<a class="item">
-			Descargas
-		</a>
-		<a class="item">
-			Acerca de
-		</a>
-		
-		<div class="item right floated">
-			<div class="ui primary labeled icon button">
-				Iniciar Sesión
-				<i class="lock icon"></i>
+	<div class="ui attached stackable menu">
+		<div class="ui container">
+			<div class="header item">
+				PokeRancher
 			</div>
-			<div class="ui button">
-				Registrarse
+			<a class="item">
+				<i class="home icon"></i> Inicio
+			</a>
+			<!--<a class="item">
+      <i class="grid layout icon"></i> Browse
+    </a>
+    <a class="item">
+      <i class="mail icon"></i> Messages
+    </a>
+    <div class="ui simple dropdown item">
+      Comunidad
+      <i class="dropdown icon"></i>
+      <div class="menu">
+        <a class="item"><i class="edit icon"></i> Edit Profile</a>
+        <a class="item"><i class="globe icon"></i> Choose Language</a>
+        <a class="item"><i class="settings icon"></i> Account Settings</a>
+      </div>   
+			</div>
+			-->
+
+			<div class="right item borderless">
+				<div class="ui labeled icon button">
+					Registrarse
+					<i class="add icon"></i>
+				</div>
+				<div class="ui primary labeled icon button" style="margin-left:20px;">
+					Iniciar Sesión
+					<i class="lock icon"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="ui fluid container bannerView">
+		<img class="ui centered rounded image" src="https://cc-media-foxit.fichub.com/image/fox-it-mondofox/a6750a7b-ab75-4d0f-998f-bf4cdaf63086/pokemon-sole-e-luna-ecco-come-ottenere-pikachu-con-il-cappellino-di-ash-maxw-654.jpg" />
+	</div>
+
+	<div class="ui container mainView">
+
+		<div class="ui internally celled segment grid">
+			<!-- esto es mejor hacerlo con container me parece -->
+			<div class="row">
+				<div class="fluid wide column">
+					<div class="ui centered panorama test ad" data-text="Panorama" style="margin-bottom: 20px;"></div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="three wide column">
+					<img src="https://image.flaticon.com/icons/svg/528/528098.svg" />
+				</div>
+				<div class="eight wide justified column">
+					<div class="showNew">
+						<h1 class="ui medium header">
+							<i class="newspaper outline icon"></i>
+							<div class="content">
+								Un Titulo aca?
+								<div class="sub header" style="font-size:0.90em">Lunes, 13 de Marzo de 2020</div>
+							</div>
+						</h1>
+						<a class="ui blue right ribbon label">Categoria</a>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id placerat dui, nec euismod augue. Nam
+							venenatis
+							dui in interdum pellentesque. Fusce interdum tempor congue. Suspendisse non odio a risus eleifend sagittis non
+							eu
+							lorem. Nunc convallis suscipit diam a tempus. Suspendisse lacinia ante elementum consequat imperdiet. Phasellus
+							sollicitudin ex diam. Nam vel convallis nisi, et dignissim velit. Sed quis magna sem. Pellentesque non nisi
+							justo.
+							Curabitur sagittis non nisl a semper. Sed vel vestibulum leo, eget tristique diam.Morbi urna orci, maximus vitae
+							nibh a, lacinia tristique massa. Nunc arcu augue, ultrices et facilisis sed, posuere vel lorem. Duis vestibulum
+							suscipit arcu, sit amet feugiat metus. Nam aliquet lorem ac rhoncus bibendum. Pellentesque habitant morbi
+							tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus
+							et netus et malesuada fames ac turpis egestas. Proin malesuada lectus a erat euismod pulvinar. Vestibulum ut
+							felis
+							ac dui malesuada finibus sit amet blandit metus. Nullam eget orci lobortis, bibendum lacus nec, faucibus leo.
+							Vivamus tempus semper tortor eu eleifend. Nam sed justo non justo sagittis efficitur. Phasellus tristique ante
+							sed
+							turpis dapibus aliquam nec id justo. Pellentesque in tincidunt mauris. Sed vel tincidunt est, sed lobortis ante.
+							Donec id sagittis est. In nulla orci, posuere at semper at, eleifend non risus. Vivamus commodo pellentesque
+							egestas. Maecenas congue tortor nec mollis aliquam. Praesent eu elit a lacus pulvinar semper. Vestibulum a
+							faucibus ex. Fusce lacinia sollicitudin nisl. Morbi semper vestibulum augue, at vehicula metus volutpat non.
+							Donec
+							tempor massa in lacus ultrices, vel scelerisque massa molestie. Praesent ac ultricies ligula, quis facilisis
+							ipsum. Quisque suscipit odio at sem molestie varius. Aenean nec dolor eget mauris tincidunt ultrices non eget
+							libero.
+						</p>
+					</div>
+
+					<div class="showNew">
+						<h1 class="ui medium header">
+							<i class="newspaper outline icon"></i>
+							<div class="content">
+								Un Titulo aca?
+								<div class="sub header" style="font-size:0.90em">Lunes, 13 de Marzo de 2020</div>
+							</div>
+						</h1>
+
+						<a class="ui blue right ribbon label">Categoria</a>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id placerat dui, nec euismod augue. Nam
+							venenatis
+							dui in interdum pellentesque. Fusce interdum tempor congue. Suspendisse non odio a risus eleifend sagittis non
+							eu
+							lorem. Nunc convallis suscipit diam a tempus. Suspendisse lacinia ante elementum consequat imperdiet. Phasellus
+							sollicitudin ex diam. Nam vel convallis nisi, et dignissim velit. Sed quis magna sem. Pellentesque non nisi
+							justo.
+							Curabitur sagittis non nisl a semper. Sed vel vestibulum leo, eget tristique diam.Morbi urna orci, maximus vitae
+							nibh a, lacinia tristique massa. Nunc arcu augue, ultrices et facilisis sed, posuere vel lorem. Duis vestibulum
+							suscipit arcu, sit amet feugiat metus. Nam aliquet lorem ac rhoncus bibendum. Pellentesque habitant morbi
+							tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus
+							et netus et malesuada fames ac turpis egestas. Proin malesuada lectus a erat euismod pulvinar. Vestibulum ut
+							felis
+							ac dui malesuada finibus sit amet blandit metus. Nullam eget orci lobortis, bibendum lacus nec, faucibus leo.
+							Vivamus tempus semper tortor eu eleifend. Nam sed justo non justo sagittis efficitur. Phasellus tristique ante
+							sed
+							turpis dapibus aliquam nec id justo. Pellentesque in tincidunt mauris. Sed vel tincidunt est, sed lobortis ante.
+							Donec id sagittis est. In nulla orci, posuere at semper at, eleifend non risus. Vivamus commodo pellentesque
+							egestas. Maecenas congue tortor nec mollis aliquam. Praesent eu elit a lacus pulvinar semper. Vestibulum a
+							faucibus ex. Fusce lacinia sollicitudin nisl. Morbi semper vestibulum augue, at vehicula metus volutpat non.
+							Donec
+							tempor massa in lacus ultrices, vel scelerisque massa molestie. Praesent ac ultricies ligula, quis facilisis
+							ipsum. Quisque suscipit odio at sem molestie varius. Aenean nec dolor eget mauris tincidunt ultrices non eget
+							libero.
+						</p>
+					</div>
+				</div>
+				<div class="five wide column">
+					<!-- Login Box -->
+					<h5>Inciar Sesion</h5>
+					<form class="ui large form">
+						<div class="ui stacked segment">
+							<div class="field">
+								<div class="ui left icon input">
+									<i class="user icon"></i>
+									<input type="text" name="email" placeholder="Usuario">
+								</div>
+							</div>
+							<div class="field">
+								<div class="ui left icon input">
+									<i class="lock icon"></i>
+									<input type="password" name="password" placeholder="Contraseña">
+								</div>
+							</div>
+							<div class="ui fluid large primary submit button">Conectar</div>
+						</div>
+
+						<div class="ui error message"></div>
+
+					</form>
+
+					<div class="ui centered skyscraper test ad" data-text="Skyscraper">
+						<!-- PokeRancher -->
+						<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7257837165893054" data-ad-slot="9779622330"
+						 data-ad-format="auto" data-full-width-responsive="true"></ins>
+						<script>
+							(adsbygoogle = window.adsbygoogle || []).push({});
+						</script>
+					</div>
+				</div>
 			</div>
 		</div>
 
 	</div>
+
+	<div class="ui inverted vertical footer segment">
+		<div class="ui container">
+			<div class="ui stackable inverted divided equal height stackable grid">
+				<div class="four wide column">
+					<h4 class="ui inverted header">About</h4>
+					<div class="ui inverted link list">
+						<a href="#" class="item">Sitemap</a>
+						<a href="#" class="item">Contact Us</a>
+						<a href="#" class="item">Religious Ceremonies</a>
+						<a href="#" class="item">Gazebo Plans</a>
+					</div>
+				</div>
+				<div class="four wide column">
+					<h4 class="ui inverted header">Services</h4>
+					<div class="ui inverted link list">
+						<a href="#" class="item">Banana Pre-Order</a>
+						<a href="#" class="item">DNA FAQ</a>
+						<a href="#" class="item">How To Access</a>
+						<a href="#" class="item">Favorite X-Men</a>
+					</div>
+				</div>
+				<div class="five wide column">
+					<h4 class="ui inverted header">Footer Header</h4>
+					<p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="semantic/semantic.min.js"></script>
+	<script src="resources/js/app.js"></script>
+
 </body>
